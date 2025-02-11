77f90254af17336294c18ff7ee6e6bae9de9221d
Nick Manning <nmannin4@kent.edu>
2025-02-11 11:06:43 -0500

    add libraries and comment out code from other section

--- 0_GetMicroMeso_in_Cerrado.R
+++ 0_GetMicroMeso_in_Cerrado.R
@@ -1,8 +1,11 @@
 # title: 0_GetMicroMeso_in_Cerrado
-
-# Use a similar workflow to other script (section 6.3 in Code 1) but intersect to end up with a list of micro regions in Cerrado 
+# author: Nick Manning
+# purpose: Use a similar workflow to other script (section 6.3 in Code 1) but intersect Cerrado shapefile and MicroRegions Shapefile to 
+# end up with a list of MicroRegions in Cerrado. This will be used in 1_data_import_clean.R to SIDRA stats at finest resolution  
 
 library(geobr)
+library(sf)
+library(dplyr)
 
 micro <- read_micro_region(
   code_micro = "all",
@@ -14,7 +17,7 @@ micro <- read_micro_region(
 
 #########################################################################
 
-## 6.3.1: Load municipality shapefile
+## 1: Load Cerrado Shapefile 
 
 # Read all municipalities in the country at a given year
 # to-do: change to shp_br_muni
@@ -24,7 +27,7 @@ micro <- read_micro_region(
 ### 6.3.2: Load Cerrado shapefile ----
 shp_br_cerr <- read_biomes(
   year = 2019,
-  simplified = T,
+  simplified = T, # simple is okay here bc we're just intersecting regions not doing stats
   showProgress = T
 ) %>% dplyr::filter(name_biome == "Cerrado")
 
@@ -34,7 +37,7 @@ str(micro)
 
 # get municipalities that are at all within the Cerrado
 shp_micro_in_cerr <- st_intersection(micro, shp_br_cerr)
-terra::plot(shp_micro_in_cerr$geom)
+# terra::plot(shp_micro_in_cerr$geom)
 
 shp_code_micro <- shp_micro_in_cerr %>% select(code_micro, geom)
 
@@ -45,19 +48,19 @@ micro_codes_cerr <- shp_micro_in_cerr$code_micro
 write.csv(micro_codes_names_cerr, "../Data_Source/microregion_codes_names_cerrado.csv", row.names = F)
 write.csv(micro_codes_cerr, "../Data_Source/microregion_codesonly_cerrado.csv", row.names = F)
 
-### 6.3.5: filter to just the territories (municipalities) within the Cerrado 
-trans_tosoy_cerrmicro <- trans_tosoy %>% 
-  filter(municipality_code %in% muni_codes_cerr)
-
-
-## 6.4: Get Land Transition to Soy  -----
-
-# Aggregate to one value per year
-# agg to one value per entire region per year
-df_trans_to_soy_BRCerr_muni <- trans_tosoy_cerrmuni %>% 
-  aggregate(trans ~ yr, ., sum) %>%
-  mutate(country = "Brazil")
-
-df_trans_to_soy_BRCerr_muni <- df_trans_to_soy_BRCerr_muni %>% filter(yr >= 2007 & yr <= 2017)
+# ### 6.3.5: filter to just the territories (municipalities) within the Cerrado 
+# trans_tosoy_cerrmicro <- trans_tosoy %>% 
+#   filter(municipality_code %in% muni_codes_cerr)
+# 
+# 
+# ## 6.4: Get Land Transition to Soy  -----
+# 
+# # Aggregate to one value per year
+# # agg to one value per entire region per year
+# df_trans_to_soy_BRCerr_muni <- trans_tosoy_cerrmuni %>% 
+#   aggregate(trans ~ yr, ., sum) %>%
+#   mutate(country = "Brazil")
+# 
+# df_trans_to_soy_BRCerr_muni <- df_trans_to_soy_BRCerr_muni %>% filter(yr >= 2007 & yr <= 2017)
 
 
